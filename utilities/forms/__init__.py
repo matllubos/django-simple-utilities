c2e78a1aaacc9499e86c7b1c6ee9b37bbd8e8647
= <matllubos@gmail.com>
2012-08-15 19:47:17 +0200

    Moved forms to __init__

--- utilities/forms/__init__.py
+++ utilities/forms/__init__.py
@@ -5,11 +5,11 @@ from django.utils.encoding import smart_str, smart_unicode
 from django import forms
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
-
-from utilities.forms.widgets import FullSizeMultipleSelectWidget,  OtherSelectWidget
-from django.forms.models import ModelChoiceIterator
+from django.forms.models import ModelChoiceIterator, ModelForm
 from django.utils.safestring import mark_safe
 from django.core import validators
+
+from utilities.forms.widgets import FullSizeMultipleSelectWidget,  OtherSelectWidget
 
 class CommaDecimalField(forms.FloatField):
 
@@ -159,3 +159,53 @@ class AutoFormatIntegerField(forms.IntegerField):
     def clean(self, value):
         value = smart_str(value).replace(' ', '')
         return super(AutoFormatIntegerField, self).clean(value)   
+    
+    
+    
+    
+    
+    
+    
+class InitialModelForm(ModelForm):
+    
+    initial_values = {}
+    
+    def __init__(self, data=None, files=None, initial=None):
+        data = self.remove_initial_values(data)
+        ModelForm.__init__(self, data=data, files=files, initial=initial)
+        self.set_initial_values()
+        
+           
+    def set_initial_values(self):
+        for key, val in self.initial_values.items():
+            if (self.fields[key].__class__.__name__ in ('TypedChoiceField', 'ModelChoiceField')):
+                choices =  list(self.fields[key].widget.choices) 
+                choices[0] = ('', val)
+                self.fields[key].widget.choices = choices
+            else:
+                self.fields[key].initial = val
+            self.fields[key].widget.attrs['title'] = val
+     
+    def remove_initial_values(self, data):
+        if (data):
+            data = data.copy()
+            for key, val in self.initial_values.items():
+                if (data.get(key) == val):
+                    data[key] = ''
+        return data
+                       
+            
+class MultiFieldsValidationModelForm(ModelForm):
+    validators = ()
+    
+    
+    def clean(self):
+        cleaned_data = super(MultiFieldsValidationModelForm, self).clean()
+        for validator in self.validators:
+            try:
+                validator().validate(cleaned_data, self)
+            except KeyError:
+                pass
+        return cleaned_data
+    
+    
