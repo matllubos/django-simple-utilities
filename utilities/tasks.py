a1c38c3954da7972d0a1d06379f4653c19e00a4c
= <matllubos@gmail.com>
2012-11-19 14:47:26 +0100

    Fixed bug in tasks

--- utilities/tasks.py
+++ utilities/tasks.py
@@ -1,10 +1,8 @@
-import StringIO
-import pickle
+import cStringIO
 
 from celery import task
 from django.db.models import get_model
 from utilities.models import GeneratedFile
-from django.contrib.contenttypes.models import ContentType
 from django.core.files.base import ContentFile
 from utilities.csv_generator import CsvGenerator
 from django.utils import translation
@@ -16,15 +14,14 @@ from django.contrib import admin
 def generate_csv(generated_file_pk, app_label, model_name, ids, language):
     try:
         generated_file = GeneratedFile.objects.get(pk = generated_file_pk)
+        translation.activate(language)
+        
         admin.autodiscover()
         model = get_model(app_label, model_name)
         model_admin = admin.site._registry[model]
-
-        
-        translation.activate(language)
-        model = get_model(app_label, model_name)
-        queryset = model.objects.filter(pk__in = ids)    
-        output = StringIO.StringIO()   
+        queryset = model.objects.filter(pk__in = ids) 
+      
+        output = cStringIO.StringIO() 
         csv_generator = CsvGenerator(model, model_admin.csv_fields, header=model_admin.csv_header, delimiter=model_admin.csv_delimiter, quotechar = model_admin.csv_quotechar,  DB_values = model_admin.csv_DB_values, csv_formatters=model_admin.csv_formatters, encoding=model_admin.csv_encoding)
         csv_generator.export_csv(output, queryset)
             
@@ -33,4 +30,3 @@ def generate_csv(generated_file_pk, app_label, model_name, ids, language):
     
     except ObjectDoesNotExist:
         pass
-    
