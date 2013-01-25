ef5c1cecb36fe3276899e376e9ae0ffbd0b878ce
Luboš Mátl <matllubos@gmail.com>
2013-01-25 14:56:09 +0100

    Added plugin to tinymce

--- utilities/static/utilities/js/models/textareas.js
+++ utilities/static/utilities/js/models/textareas.js
@@ -1,3 +1,98 @@
+(function() {
+    
+    // walk the editors dom and insert special chars 
+    // or revert what was done before depending on toggle_mode
+    function toggleContents(node, toggle_mode) {
+
+        for (var i=0; i<node.childNodes.length; i++) {
+            el = node.childNodes[i];
+            if (el.nodeType == 3) { 
+                if (toggle_mode) {
+                    el.nodeValue = el.nodeValue.replace(/\u00b7\u200b/g, ' ');
+                }
+                else {
+                    el.nodeValue = el.nodeValue.replace(/\ /g, '\u00b7\u200b');
+                }
+            }
+            else if (el.nodeName.toLowerCase() == 'br'){
+                //console.log("br found");
+                var bogus = el.hasAttribute ? el.hasAttribute('_mce_bogus') : el.getAttribute('_mce_bogus');
+                if (!toggle_mode && !bogus) { // \u21B5
+                    el.parentNode.insertBefore(document.createTextNode('\u21B5'),el);
+                    i++;
+                }
+                else {
+                    sibling = el.previousSibling;
+                    if (sibling && sibling.nodeType == 3) sibling.nodeValue = sibling.nodeValue.replace(/\u21B5/g, '');
+                }
+            }
+            else if (el.nodeName.toLowerCase() == 'p'){
+                //console.log("p found");
+                
+                if (!toggle_mode) { // \u00b6
+                    chr = '\u00b6';
+                    el.appendChild(document.createTextNode(chr));
+                }
+                else {
+                    if(el.lastChild && el.lastChild.nodeType == 3) el.lastChild.nodeValue = el.lastChild.nodeValue.replace(/\u00b6/g, '');
+                }
+            }
+            if (el.childNodes.length > 0) { toggleContents(el, toggle_mode); }
+        }
+        return 1;
+    }
+                
+    tinymce.create('tinymce.plugins.hiddenchars', {
+        
+        init : function(ed, url) {
+
+            // Register hiddenchars button
+            ed.addButton('hiddenchars', {
+                title : 'Toggle hidden characters',
+                cmd : 'mce_toggle_hidden_chars',
+            });
+            
+            ed.onInit.add(function(ed) {
+                ed.hiddenchars_toggled = false;
+            });
+            
+            function toggleback(ed){
+                if (ed.hiddenchars_toggled) {
+                    var root = ed.getBody();
+                    toggleContents(root, true);
+                    ed.hiddenchars_toggled = false;
+                    ed.controlManager.setActive('hiddenchars', ed.hiddenchars_toggled);
+                }
+            };
+            
+            ed.onKeyDown.add(function(ed) {
+                toggleback(ed);
+            });
+            
+            ed.onClick.add(function(ed){
+                toggleback(ed);
+            });
+            
+            
+            ed.addCommand('mce_toggle_hidden_chars', function() {
+                var root = ed.getBody();
+                toggleContents(root,ed.hiddenchars_toggled);
+                
+                                
+                ed.hiddenchars_toggled = !ed.hiddenchars_toggled;
+                ed.controlManager.setActive('hiddenchars', ed.hiddenchars_toggled);
+
+            });
+        },
+    });
+    // Register plugin
+    tinymce.PluginManager.add('hiddenchars', tinymce.plugins.hiddenchars);
+
+})();
+
+
+
+
 tinyMCE.init({
 	mode : "specific_textareas",
     editor_selector : /(tinymce)/,
@@ -9,11 +104,11 @@ tinyMCE.init({
 	theme_advanced_path : false, 
 	theme_advanced_resizing : true, 
 	theme_advanced_resizing_use_cookie : true,
-	theme_advanced_buttons1 : "fullscreen,removeformat,separator,preview,separator,bold,italic,underline,strikethrough,separator,bullist,numlist,outdent,indent,separator,undo,redo,separator,link,unlink,anchor,separator,image,media,cleanup,help,separator,code",
+	theme_advanced_buttons1 : "fullscreen,removeformat,separator,preview,separator,bold,italic,underline,strikethrough,separator,bullist,numlist,outdent,indent,separator,undo,redo,separator,link,unlink,anchor,separator,image,media,cleanup,help,code,hiddenchars",
 	theme_advanced_buttons2 : "",
 	theme_advanced_buttons3 : "",
 	auto_cleanup_word : true,
-	plugins : "fullscreen, tabfocus",
+	plugins : "fullscreen, tabfocus, hiddenchars",
 	plugin_insertdate_dateFormat : "%m/%d/%Y",
 	plugin_insertdate_timeFormat : "%H:%M:%S",
 valid_elements : "@[id|class|style|title|dir<ltr?rtl|lang|xml::lang|onclick|ondblclick|"
@@ -41,7 +136,7 @@ valid_elements : "@[id|class|style|title|dir<ltr?rtl|lang|xml::lang|onclick|ondb
 	fullscreen_settings : {
 		theme_advanced_path_location : "top",
 		theme_advanced_buttons1 : "fullscreen,separator,preview,separator,media,cut,copy,paste,separator,undo,redo,separator,search,replace,separator,code,separator,cleanup,separator,bold,italic,underline,strikethrough,separator,forecolor,backcolor,separator,justifyleft,justifycenter,justifyright,justifyfull,separator,help",
-		theme_advanced_buttons2 : "removeformat,styleselect,formatselect,fontselect,fontsizeselect,separator,bullist,numlist,outdent,indent,separator,link,unlink,anchor",
+		theme_advanced_buttons2 : "removeformat,styleselect,formatselect,fontselect,fontsizeselect,separator,bullist,numlist,outdent,indent,hiddenchars,link,unlink,anchor",
 		theme_advanced_buttons3 : "sub,sup,separator,image,insertdate,inserttime,separator,tablecontrols,separator,hr,advhr,visualaid,separator,charmap,emotions,iespell,flash,separator,print"
 	}
 });
